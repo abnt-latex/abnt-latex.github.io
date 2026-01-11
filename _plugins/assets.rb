19f10efb4dce94da73315bc1baa1be19807ec502
guilhermeivo <guilhermeivob@gmail.com>
2026-01-11 20:38:39 -0300

    update plugins

--- _plugins/assets.rb
+++ _plugins/assets.rb
@@ -2,13 +2,23 @@ require 'fileutils'
 
 module Jekyll
   Jekyll::Hooks.register :site, :post_write do |site|
-    destination = site.dest
-    input_file = "#{destination}/assets/logo.svg"
-    output_file = "#{destination}/favicon.ico"
+    destination = site.dest || "_site"
+    config_icon_svg = site.config['icon_svg'] || "icon.svg"
+    config_icon_ico = site.config['icon_ico'] || "favicon.ico"
+
+    input_file = "#{destination}/#{config_icon_svg}"
+    output_file = "#{destination}/#{config_icon_ico}"
     system_command = "convert -density 256x256 -background transparent #{input_file} -define icon:auto-resize #{output_file}"
 
-    puts "Jekyll Assets: Generating favicon for site"
-
-    system(system_command)
+    response = false
+    begin
+      response = system(system_command)
+    rescue
+      puts "\t\"ImageMagick\" is required to create the favicon"
+    ensure
+      if response
+        puts "\tJekyll Assets: Generating favicon for site"
+      end
+    end
   end
 end
