3237df6b363c6ee1a0a7b8ebf814f0ab59393fc4
guilhermeivo <guilhermeivob@gmail.com>
2026-01-11 21:10:12 -0300

    update assets plugin

--- _plugins/assets.rb
+++ _plugins/assets.rb
@@ -2,11 +2,23 @@ require 'fileutils'
 
 module Jekyll
   Jekyll::Hooks.register :site, :post_write do |site|
+    response = false
+    exception_message = "\t\"ImageMagick\" is required to create the favicon"
+    begin
+      response = system("convert --version")
+    rescue
+      raise exception_message
+    ensure
+      if !response
+        raise exception_message
+      end
+    end
+
     destination = site.dest || "_site"
-    config_icon_svg = site.config['icon_svg'] || "icon.svg"
+    config_logo = site.config['logo'] || "logo.svg"
     config_icon_ico = site.config['icon_ico'] || "favicon.ico"
 
-    input_file = "#{destination}/#{config_icon_svg}"
+    input_file = "#{destination}/#{config_logo}"
     output_file = "#{destination}/#{config_icon_ico}"
     system_command = "convert -density 256x256 -background transparent #{input_file} -define icon:auto-resize #{output_file}"
 
@@ -14,7 +26,7 @@ module Jekyll
     begin
       response = system(system_command)
     rescue
-      puts "\t\"ImageMagick\" is required to create the favicon"
+      puts "An error occurred"
     ensure
       if response
         puts "\tJekyll Assets: Generating favicon for site"
@@ -23,4 +35,21 @@ module Jekyll
       end
     end
   end
+
+  class SeoTag < Liquid::Tag
+    def initialize(tag_name, text, tokens)
+      super
+      @text = text
+    end
+
+    def render(context)
+      description = context.registers[:site].config["description"]
+      output = "#{@text}"
+      output += "#{!description.empty? ? "<meta name=\"description\" content=\"#{description}\">" : ""}"
+
+      output
+    end
+  end
 end
+
+Liquid::Template.register_tag('seo', Jekyll::SeoTag)
